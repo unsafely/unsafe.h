c624bb61ce248381fcebd1f5cb792e878855918c
Kimmy Leo <kenpusney@gmail.com>
2017-08-08 15:54:48 +0800

    add examples

---
+++ example/example.cpp
@@ -0,0 +1,25 @@
+#include <unsafe.h>
+
+unsafe struct s
+{
+    double f1;
+    unsafe int f2;
+};
+
+unsafe int get_f2(struct s);
+
+unsafe int get_f2(struct s s1)
+{
+    unsafe
+    {
+        return s1.f2;
+    }
+}
+
+unsafe int main()
+{
+    struct s s1;
+    unsafe int i1 = get_f2(s1);
+
+    return i1;
+}
